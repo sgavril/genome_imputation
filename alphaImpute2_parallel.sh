1121681151186b7f5f50bf5ccfeb3ba614934a74
sgavril <stefan.gavriliuc@ucalgary.ca>
2023-08-07 23:44:49 -0600

    fix: corrected check for existing imp file

--- alphaImpute2_parallel.sh
+++ alphaImpute2_parallel.sh
@@ -13,12 +13,14 @@ source venv3.6/bin/activate
 #mkdir -p /scratch/20708102/outputs2
 
 find /scratch/20708102/replicates2/ -type f | grep ".bim$" | \
-	parallel -j 48 \
-		'if [ ! -f /scratch/20708102/outputs2/{/.} ]; then \
+	parallel \
+		'if [ ! -f /scratch/20708102/outputs2/{/} ]; then \
 			AlphaImpute2 \
 			-maxthreads 2 \
 			-bfile {.} \
 			-pop_only \
 			-binaryoutput \
 			-out /scratch/20708102/outputs2/{/.}; \
+		else \
+			echo "File /scratch/20708102/outputs2/{/.} already exists, skipping"; \
 		fi'
