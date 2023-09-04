e759e2cdc7a9666a7f0c90a491eb8ef10140fccd
sgavril <stefan.gavriliuc@ucalgary.ca>
2023-09-03 19:04:55 -0600

    feat: added update script to append imputed position indices to hdf5

--- convert2hdf5.py
+++ convert2hdf5.py
@@ -1,28 +1,32 @@
-import os, h5py
+import os, h5py, numpy as np
 from pandas_plink import read_plink
+
 
 def write_to_hdf5(hdf5_filename, data_dict):
     """Write genotype data to HDF5 file."""
     with h5py.File(hdf5_filename, 'w') as f:
         for key, value in data_dict.items():
             fields = key.split('_')
+            print(fields)
             sample_name = '_'.join(fields[:3])
-            num_snps = fields[4]
-            snp_sel_method = fields[5]
+            num_snps = fields[3]
+            snp_sel_method = fields[4]
+            num_ind = fields[5]
             replicate = fields[6]
 
-            group_name = f"{sample_name}/{num_snps}/{snp_sel_method}/{replicate}"
+            group_name = f"{sample_name}/{num_snps}/{snp_sel_method}/{num_ind}/{replicate}"
+            print(group_name)
             if group_name not in f:
                 group = f.create_group(group_name)
             else:
                 group = f[group_name]
 
             value = value.astype('int')
-            f.create_dataset(key, data=value, chunks=True, compression='gzip', compression_opts=9)
+            group.create_dataset('SNPs', data=value, chunks=True, compression='gzip', compression_opts=9)
 
 if __name__ == '__main__':
     # Specify the directory containing PLINK Files
-    plink_dir = 'outputs'
+    plink_dir = 'replicates'
 
     all_genotypes = {}
 
--- inspect_h5.py
+++ inspect_h5.py
@@ -1,21 +1,39 @@
 import h5py
 
-# Function to recursively print groups and datasets
-def print_hdf5_item_structure(g, offset='    '):
-    """ Print the input file/group/dataset name and begin iterations over its content """
-    if isinstance(g, h5py.File):
-        print(g.file, '(File)', g.name)
-    elif isinstance(g, h5py.Dataset):
-        print('(Dataset)', g.name, '    len =', g.shape)  # , g.dtype
-    elif isinstance(g, h5py.Group):
-        print('(Group)', g.name)
-
-    if isinstance(g, h5py.File) or isinstance(g, h5py.Group):
-        for key, val in dict(g).items():
-            subg = val
-            print(offset, key)  # , '    ', subg.name # , val, subg.len(), type(subg),
-            print_hdf5_item_structure(subg, offset + '    ')
-
-# Open the existing HDF5 file
+# Open the HDF5 file in read mode
 with h5py.File('all_genotypes.h5', 'r') as f:
-    print_hdf5_item_structure(f)
+    # Loop through each sample_name
+    for sample_name in f.keys():
+        print(f"Checking sample: {sample_name}")
+        sample_group = f[sample_name]
+        
+        # Loop through each num_snps
+        for num_snps in sample_group.keys():
+            print(f"  Checking num_snps: {num_snps}")
+            num_snps_group = sample_group[num_snps]
+            
+            # Loop through each snp_sel_method
+            for snp_sel_method in num_snps_group.keys():
+                print(f"    Checking snp_sel_method: {snp_sel_method}")
+                snp_sel_method_group = num_snps_group[snp_sel_method]
+                
+                # Loop through each num_ind
+                for num_ind in snp_sel_method_group.keys():
+                    print(f"    Checking num_ind: {num_ind}")
+                    num_ind_method_group = num
+
+                # Loop through each replicate
+                for replicate in snp_sel_method_group.keys():
+                    print(f"      Checking replicate: {replicate}")
+                    replicate_group = snp_sel_method_group[replicate]
+                    
+                    # Check if 'Masked_Positions' dataset exists under this replicate
+                    if 'Masked_Positions' in replicate_group:
+                        # Read the dataset into a NumPy array
+                        masked_positions = replicate_group['Masked_Positions'][:]
+                        
+                        # Print the masked positions
+                        print(f"Masked Positions for {sample_name}/{num_snps}/{snp_sel_method}/{replicate}:")
+                        print(masked_positions)
+                    else:
+                        print(f"      No 'Masked_Positions' in {replicate}")
