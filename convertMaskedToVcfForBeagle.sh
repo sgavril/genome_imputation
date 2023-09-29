d4e419faeaeb7c393be55892520a9db569df5301
sgavril <stefan.gavriliuc@ucalgary.ca>
2023-09-28 23:02:37 -0600

    feat: processing beagle (plink -> vcf -> plink)

--- convertMaskedToVcfForBeagle.sh
+++ convertMaskedToVcfForBeagle.sh
@@ -12,11 +12,6 @@ source venv3.6/bin/activate
 # mkdir -p /scratch/20708102/outputs_beagle
 # mkdir -p outputs_beagle
 
-# find replicates/ -type f | grep ".bim$" |
-#     parallel \
-#         './plink --bfile {.} --recode vcf --out {.} --horse && \
-#         gzip {.}".vcf" '
-
 # Just testing one
 # ./plink --bfile /scratch/20708102/replicates/SI_hair_128_10000_bpEQ_ind100_rep1 \
 #     --recode vcf \
@@ -36,20 +31,35 @@ source venv3.6/bin/activate
 #         gzip {.}".vcf" '
 
 # Just testing one iteration
-java -jar beagle.22Jul22.46e.jar ne=50 \
-    gt=replicates/SI_hair_128_10000_bpEQ_ind100_rep1.vcf.gz \
-    out=outputs_beagle/SI_hair_128_10000_bpEQ_ind100_rep1.vcf
+# java -jar beagle.22Jul22.46e.jar ne=50 \
+#     gt=replicates/SI_hair_128_10000_bpEQ_ind100_rep1.vcf.gz \
+#     out=outputs_beagle/SI_hair_128_10000_bpEQ_ind100_rep1.vcf
 
-# find replicates3/ -type f | grep ".vcf.gz$" |
-#     parallel -j 4 \
-#         'base=$(basename {}); \
-#         base_no_ext="${base%.gz}"; \
-#         base_no_ext="${base_no_ext%.vcf}"; \
-#         echo "Debug: base=$base, base_no_ext=$base_no_ext"; \
-#         echo "java -jar beagle.22Jul22.46e.jar gt={} ne=50 out=outputs_beagle/$base_no_ext" ; \
-#         if [ ! -f outputs_beagle/$base ]; then \
-#             echo "Output file not found ; running beagle"; \
-#             java -jar beagle.22Jul22.46e.jar gt={} ne=50 out=outputs_beagle/$base_no_ext; \
-#         else \
-#             echo "File outputs_beagle/$base already exists, skipping."; \
-#         fi'
+# Take the replicate files and covert to vcf for plink
+# Note that recode munges family id + sample id, so use sed to remove fid
+rm replicates3/*.vcf.gz 
+find replicates3/ -type f | grep ".bim$" |
+    parallel \ 
+        './plink --bfile {.} --recode vcf --out {.} --horse && \
+        sed -i "s/Sable_//g" {.}.vcf && gzip {.}.vcf '
+
+# Run beagle using parallel
+find replicates3/ -type f | grep ".vcf.gz$" |
+    parallel -j 4 \
+        'base=$(basename {}); \
+        base_no_ext="${base%.gz}"; \
+        base_no_ext="${base_no_ext%.vcf}"; \
+        echo "Debug: base=$base, base_no_ext=$base_no_ext"; \
+        echo "java -jar beagle.22Jul22.46e.jar gt={} ne=50 out=outputs_beagle3/$base_no_ext" ; \
+        if [ ! -f outputs_beagle3/$base ]; then \
+            echo "Output file not found ; running beagle"; \
+            java -jar beagle.22Jul22.46e.jar gt={} ne=50 out=outputs_beagle3/$base_no_ext; \
+        else \
+            echo "File outputs_beagle3/$base already exists, skipping."; \
+        fi'
+
+# Re-convert back to plink genotype files for accuracy calcs
+find outputs_beagle3/ -type f -name "*.vcf.gz" | while read -r vcf_file; do
+    output_prefix=$(basename "${vcf_file}" .vcf.gz)
+    ./plink --vcf "${vcf_file}" --make-bed --const-fid --horse --out "outputs_beagle3/${output_prefix}"
+done
