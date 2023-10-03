978ce47ab53e449157353eea3b5fc14a8dc226b1
sgavril <stefan.gavriliuc@ucalgary.ca>
2023-10-02 18:04:49 -0600

    chore: file org

--- plink_packages_did_not_work/plink_alpha.py
+++
@@ -1,7 +0,0 @@
-import alphaplinkpython.PlinkWriter as pw
-
-bim = pw.readBimFile("../loo/SI_hair_128_10000_bpEQ.bim")
-fam = pw.readFamFile("../loo/SI_hair_128_10000_bpEQ.fam")
-bed = pw.readBedFile("../loo/SI_hair_128_10000_bpEQ.bed", bim, fam)
-
-pw.writeBedFile(bed, 'test.bed')
