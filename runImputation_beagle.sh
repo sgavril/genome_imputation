69c1593f41272edfd925a7ab69b0920b44153a34
sgavril <stefan.gavriliuc@ucalgary.ca>
2023-10-08 16:26:13 -0600

    fix: beagle job

--- runImputation_beagle.sh
+++ runImputation_beagle.sh
@@ -10,7 +10,7 @@
 #SBATCH --array=1-100
 
 # Create the output and timing directories if they don't exist
-rm /scratch/23176676/outputs_beagle/* ; rm /scratch/23176676/time_logs_beagle/*
+# rm /scratch/23176676/outputs_beagle/* ; rm /scratch/23176676/time_logs_beagle/*
 mkdir -p /scratch/23176676/outputs_beagle/
 mkdir -p /scratch/23176676/time_logs_beagle/
 
@@ -21,27 +21,21 @@ FILES_PER_JOB=$((TOTAL_FILES / 100))
 START=$(( (SLURM_ARRAY_TASK_ID - 1) * FILES_PER_JOB + 1 ))
 END=$(( START + FILES_PER_JOB - 1 ))
 
-# Calculate start and end indices for this array job
-TOTAL_FILES=${#FILES[@]}
-FILES_PER_JOB=$((TOTAL_FILES / 100))
-START=$(( (SLURM_ARRAY_TASK_ID - 1) * FILES_PER_JOB ))
-END=$(( START + FILES_PER_JOB - 1 ))
-
 # Extract the files for this array job
 sed -n "$START,$END p" data/file_list_vcfs.txt > data/current_files_vcfs_$SLURM_ARRAY_TASK_ID.txt
 
 echo "Starting GNU parallel to run Beagle..."
 
 # Process the subset of files for this array job
-parallel -j 50 --dry-run --joblog /scratch/23176676/time_logs_beagle/joblog_$SLURM_ARRAY_TASK_ID.log \
+parallel -j 4 --joblog /scratch/23176676/time_logs_beagle/joblog_$SLURM_ARRAY_TASK_ID.log \
     'FILE={}; \
     BASENAME=$(basename $FILE .vcf); \
     echo "DEBUG: Processing $FILE with basename ${BASENAME}"; \
     OUTPUT_FILE="/scratch/23176676/outputs_beagle/${BASENAME}";  \
     TIME_LOG="/scratch/23176676/time_logs_beagle/${BASENAME}.time.log"; \
     (time java -jar beagle.22Jul22.46e.jar \
-        gt=${FILE%.*} \
+        gt=$FILE \
         ne=50 \
-        out= $OUTPUTFILE) &> $TIME_LOG' :::: data/current_files_vcfs_$SLURM_ARRAY_TASK_ID.txt
+        out=$OUTPUT_FILE) &> $TIME_LOG' :::: data/current_files_vcfs_$SLURM_ARRAY_TASK_ID.txt
 
 rm data/current_files_vcf_$SLURM_ARRAY_TASK_ID.txt
